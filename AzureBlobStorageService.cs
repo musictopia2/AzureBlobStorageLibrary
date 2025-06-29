f18364e083262e4095a9ad695a808444128cc8bb
Andy Walter <musictopia2@hotmail.com>
2025-06-29 08:19:50 -0700

    Add project files.

---
+++ AzureBlobStorageService.cs
@@ -0,0 +1,28 @@
+﻿namespace AzureBlobStorageLibrary;
+public class AzureBlobStorageService(BlobContainerClient containerClient) : IBlobStorageService
+{
+    Task IBlobStorageService.DeleteAsync(string blobName)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<Stream> IBlobStorageService.DownloadAsync(string blobName)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<bool> IBlobStorageService.ExistsAsync(string blobName)
+    {
+        throw new NotImplementedException();
+    }
+
+    Uri IBlobStorageService.GetUri(string blobName)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task IBlobStorageService.UploadAsync(string blobName, Stream data)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.BlobStorage;
+global using Azure.Storage.Blobs;
+global using Microsoft.Extensions.DependencyInjection;
+global using bb1 = CommonBasicLibraries.BasicDataSettingsAndProcesses.BasicDataFunctions;
+global using Azure.Storage.Blobs.Models;
+global using CommonBasicLibraries.BasicDataSettingsAndProcesses;
+global using Microsoft.Extensions.Configuration;
